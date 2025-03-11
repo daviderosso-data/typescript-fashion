7150955ae0bad4e133abf6203063efae08e69d8b
daviderosso-data <daviderosso.data@gmail.com>
2025-03-11 21:23:42 +0100

    1.0.0 stable

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# typescript-fashion
-typescript-Fashion
+# typescript-fashion  Start2Impact 
+Progetto didattico del Corso Start2Impact
+Marzo 2025
+
+## Progetto
+
+Il progetto prevede un implementazione base di alcune classi in typescript per affrontare la logica e la sintassi di questo linguaggio.
+
+## Cliente
+
+Sunnee
+Brand di beachwear sostenibile
+
+## Struttura
+
+Il progetto prevede un unico file typescript in cui sono presenti 3 classi per la produzione, la vendita e la gestione dei clienti.
+Non é prevista un interfaccia, ho cercato di rendere piú gradevole la visualizzazione su Console.
+
+Si richiede inoltre l'upload su CodePen.
+
+### Indirizzo CodePen
+
+https://codepen.io/daviderosso-data/pen/JojrOEw
+
--- dist/app.js
+++ dist/app.js
@@ -1,4 +1,92 @@
 "use strict";
-let number = 55;
-let message = 'Hell, World!';
-console.log(message, number);
+// Classe prodotto
+class Prodotto {
+    constructor(type, ID, size, quantity) {
+        this.type = type;
+        this.ID = ID;
+        this.size = size;
+        this.quantity = quantity;
+    }
+    assegnaCliente(cliente, pzOrdinati) {
+        this.cliente = cliente;
+        console.log(`il cliente ${cliente.name} é stato assegnato al prodotto ${this.type} ed é stato venduto`);
+        console.log(`il cliente ${cliente.name} ha ordinato la seguente quantitá di ${this.type}: ${pzOrdinati} `);
+        this.quantity = this.quantity - pzOrdinati;
+        console.log(`la quantità di ${this.type} in stock é ora di ${this.quantity}`);
+    }
+}
+// Classe Cliente
+class Cliente {
+    constructor(name, surname, email, payMethod) {
+        this.name = name;
+        this.surname = surname;
+        this.email = email;
+        this.payMethod = payMethod;
+    }
+    ordinaProdotto(prodotto, pzOrdinati) {
+        prodotto.assegnaCliente(this, pzOrdinati);
+    }
+}
+// Classe processoProduzione
+class processoProduzione {
+    constructor(processName, description) {
+        this.processName = processName;
+        this.description = description;
+        this.productInProduction = [];
+    }
+    aggiungiProdotto(prodotto, pzProdotti) {
+        if (this.processName === 'pronto alla vendita') {
+            prodotto.quantity = prodotto.quantity + pzProdotti;
+            console.log(`N. ${pzProdotti} di ${prodotto.type} ha completato la fase di ${this.processName}`);
+            console.log(`la quantità di ${prodotto.type} in stock é ora di ${prodotto.quantity}`);
+        }
+        else {
+            console.log(`N. ${pzProdotti} di ${prodotto.type} ha completato la fase di ${this.processName}`);
+        }
+    }
+}
+// istanzio prodotti, clienti e processi produttivi
+let costumeDaUomo = new Prodotto('costume da uomo', 110, 'M', 3); // Prodotto costume da uomo, codice 110, taglia M, 3 pezzi giá a magazzino
+let costumeDaDonna = new Prodotto('bikini', 123, 'S', 0); // Prodotto costume da donna, codice 123, taglia s, 0 pezzi  a magazzino
+let infraditoBlu = new Prodotto('infradito Blu', 335, '39', 20); // Prodotto infradito, codice 335, taglia 39, 20 pezzi giá a magazzino
+let cliente1 = new Cliente('mario', 'rossi', 'mariorossi@gmail.com', 'VISA');
+let cliente2 = new Cliente('sandra', 'verdi', 'sandraverdi@gmail.com', 'MASTERCARD');
+let processo1 = new processoProduzione('cucitura', 'produzione');
+let processo2 = new processoProduzione('confezionamento', 'produzione');
+let processo3 = new processoProduzione('pronto alla vendita', 'consegna retail');
+console.log('Benvenuto in Sunnee!');
+// Produco i prodotti
+// Processo di produzione Costume da uomo 
+console.log('Processo di produzione Costume da uomo');
+console.log('--------------------------------------');
+processo1.aggiungiProdotto(costumeDaUomo, 25); // cucitura
+processo2.aggiungiProdotto(costumeDaUomo, 25); // confezionamento
+processo3.aggiungiProdotto(costumeDaUomo, 24); // pronto alla vendita in negozio, un prodotto era fallato
+// Processo di produzione Costume da Donna
+console.log('--------------------------------------');
+console.log('Processo di produzione Costume da donna');
+console.log('--------------------------------------');
+processo1.aggiungiProdotto(costumeDaDonna, 12); // cucitura
+processo2.aggiungiProdotto(costumeDaDonna, 10); // confezionamento, due prodotti fallati
+processo3.aggiungiProdotto(costumeDaDonna, 10); // pronto alla vendita in negozio
+// Processo di produzione Infradito
+console.log('--------------------------------------');
+console.log('Processo di produzione Infradito');
+console.log('--------------------------------------');
+processo2.aggiungiProdotto(infraditoBlu, 8); // confezionamento
+processo3.aggiungiProdotto(infraditoBlu, 8); // pronto alla vendita in negozio
+console.log('--------------------------------------');
+console.log('Fine processo di produzione');
+console.log('--------------------------------------');
+// Vendo i prodotti
+// Processo di vendita Cliente 1
+console.log('--------------------------------------');
+console.log('Processo di vendita Cliente 1');
+console.log('--------------------------------------');
+cliente1.ordinaProdotto(costumeDaUomo, 1); // il cliente 1 compra un costume da uomo
+// Processo di vendita Cliente 2
+console.log('--------------------------------------');
+console.log('Processo di vendita Cliente 2');
+console.log('--------------------------------------');
+cliente2.ordinaProdotto(costumeDaDonna, 2); // il cliente 2 compra 2 costumi da donna e un infradito
+cliente2.ordinaProdotto(infraditoBlu, 1);
